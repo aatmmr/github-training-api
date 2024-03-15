95bfa3c97072f69be445da3e80d611b8fb072ffa
Maik Mueller <15815012+aatmmr@users.noreply.github.com>
2024-03-15 11:42:01 +0100

    refactor(octokit): polishing demo code

--- src/octokit/script.js
+++ src/octokit/script.js
@@ -1,22 +1,50 @@
 import { Octokit } from "octokit";
 import dotenv from "dotenv";
 
+// Get environment variables
 dotenv.config();
 
+// Initialize Octokit
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
+// Get authenticated user
 const {
   data: { login },
 } = await octokit.rest.users.getAuthenticated();
 
+// Use GraphQL API for same purpose
+// const {
+//   viewer: { login },
+// } = await octokit.graphql(`{
+//   viewer {
+//     login
+//   }
+// }`);
+
+// Get repositories
 const { data: repositories } = await octokit.rest.repos.listForAuthenticatedUser({
-  per_page: 10,
+  per_page: 5,
 });
 
-console.log("Hello, %s", login);
+console.log("👋 Hello, %s", login);
 
-console.log("Some of your repositories are:");
+console.log("\r\n📦 Some of your repositories are:");
 
+// List repositories
 for (const { name } of repositories) {
   console.log("- %s", name);
+}
+
+// Get issues in github-api-training repository
+const { data: issues } = await octokit.rest.issues.listForRepo({
+  owner: login,
+  repo: "github-training-api",
+  per_page: 5,
+});
+
+console.log("\r\n🎫 Some issues in github-training-api repository are:");
+
+// List issues
+for (const { title } of issues) {
+  console.log("- %s", title);
 }
